2527ca110a4678aa07012cad5af521e69257f78d
JulianMunoz9 <julian5270@outlook.com>
2024-08-20 20:00:06 -0500

    continuacion de la pagina

--- tienda/productos/forms.py
+++ tienda/productos/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Producto, Cliente
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm     #formulario de creacion de usuarios
 
 class ProductoModelForm(forms.ModelForm):
     class Meta:
@@ -9,7 +11,7 @@ class ProductoModelForm(forms.ModelForm):
     #validacion personalizada para el campo de precio
     def clean_precio(self):
         precio = self.cleaned_data.get('precio')
-        if precio <= 0: #con esta validacion nos aseguramos que aparezca un error si el precio es menor a cero.
+        if precio <= 0:     #con esta validacion nos aseguramos que aparezca un error si el precio es menor a cero.
             raise forms.ValidationError('usted que piensa de la vida colocando precios en negativo? ')
         return precio
 
@@ -23,3 +25,11 @@ class ClienteModelForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nombre', 'email', 'telefono']
+
+
+class RegistroUsuarioForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
